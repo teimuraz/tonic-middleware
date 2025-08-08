504bee521194d925199341f4cdde490d00a2e695
Σrebe - Romain GERARD <erebe@erebe.eu>
2025-08-08 17:09:21 +0200

    bump to support tonic version 0.14

--- integration_tests/build.rs
+++ integration_tests/build.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_server(true)
         .include_file("util.rs")
         .out_dir(out_dir.clone())
